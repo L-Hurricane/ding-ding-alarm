6aaf9c691653d40946e1cf36e6a7a5c943a30929
limingyuan <mingyuan.lee.xpp@gmail.com>
2018-11-09 16:58:45 +0800

    int git repository

---
+++ alarm-core/src/main/java/com/mingyuan/gaea/DingDingAlarmApplication.java
@@ -0,0 +1,15 @@
+package com.mingyuan.gaea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author limingyuan
+ */
+@SpringBootApplication
+public class DingDingAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingDingAlarmApplication.class, args);
+    }
+}
