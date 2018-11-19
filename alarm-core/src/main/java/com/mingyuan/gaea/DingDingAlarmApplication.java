950a9ea445ccb7b5b138101b815f7e33ccd6b4f3
limingyuan <mingyuan.lee.xpp@gmail.com>
2018-11-19 12:26:15 +0800

    push ding ding notification on schedule

--- alarm-core/src/main/java/com/mingyuan/gaea/DingDingAlarmApplication.java
+++ alarm-core/src/main/java/com/mingyuan/gaea/DingDingAlarmApplication.java
@@ -2,11 +2,13 @@ package com.mingyuan.gaea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author limingyuan
  */
 @SpringBootApplication
+@EnableScheduling
 public class DingDingAlarmApplication {
 
     public static void main(String[] args) {
